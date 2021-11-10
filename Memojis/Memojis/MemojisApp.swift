529e17c70d8e54e46dd6fcad4554201396416a67
Aleksandr Kuznetsov <61458847+mankofeman@users.noreply.github.com>
2021-11-10 13:53:37 +0300

    Main part

---
+++ Memojis/Memojis/MemojisApp.swift
@@ -0,0 +1,19 @@
+//
+//  MemojisApp.swift
+//  Memojis
+//
+//  Created by Alex Smith on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemojisApp: App {
+    private let game = EmojiMemoryGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiMemoryGameView(game: game)
+        }
+    }
+}
